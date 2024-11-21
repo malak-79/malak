20d3931c9112dfe83b4200b6555eaddd948b9935
malak-79 <malakbemy@gmail.com>
2024-11-21 14:51:05 +0100

    init

---
+++ evaluationloops.js
@@ -0,0 +1,11 @@
+//exe 1
+for(let n=1;n<=30;n++){
+    if(n % 2 !==0){
+console.log(n);
+    }
+}
+//exe 2
+let fruits=["apple","banana","cherry"];
+fruits.forEach(function(fruits) {
+    console.log(fruits.toUpperCase(fruits));
+});
